8f480ce9f911d6bba647dd274d54c992d0e6bdd3
vipugethanv <vipugethan.vicknesswaran@circles.asia>
2022-08-01 10:41:55 +0530

    first_commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import 'index.css';
+
+
+const HelloWorld = () => {
+    //<link rel="stylesheet" href="index.css"></link>
+    return(
+        
+    <div class="centre">
+    <h1>Login to pick a Challenge</h1>
+    <form method='post'>
+
+        <div class = "loginpage">
+            <label>Username </label>
+            <input type="text" required/>
+        </div>
+
+        <div class = "loginpage">
+            <label>Password </label>
+            <input type="password" required/>
+        </div>
+
+        
+            <input type="submit" value="Login"/><br></br><br></br>
+       
+    </form> 
+    </div>
+    
+    )
+    ;
+    
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
